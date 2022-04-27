768f0ef4e3865549cbbbdbccbce6ab48ffd4ea84
Chris-Tate94 <chris_tate_94@hotmail.com>
2022-04-27 10:52:24 -0400

    dom practice

---
+++ DOM_Intro_CODE/Manipulating/app.js
@@ -0,0 +1,25 @@
+const allLinks = document.querySelectorAll("a");
+
+// for (let link of allLinks) {
+//     link.innerText = 'I AM A LINK!!!!'
+// }
+
+for (let link of allLinks) {
+  link.style.color = "rgb(0, 108, 134)";
+  link.style.textDecorationColor = "magenta";
+  link.style.textDecorationStyle = "wavy";
+}
+
+const allImages = document.getElementsByTagName("img");
+const squareImages = document.getElementsByClassName("square");
+
+// for (const image of squareImages) {
+//   image.src =
+//     "https://images.unsplash.com/photo-1563281577-a7be47e20db9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80";
+// }
+
+// for (const image of allImages) {
+//   console.log(image.src);
+//   image.src =
+//     "https://images.unsplash.com/photo-1563281577-a7be47e20db9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80";
+// }
